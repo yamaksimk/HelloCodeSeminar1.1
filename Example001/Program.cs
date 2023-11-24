38724127d89c6324f31ea7325debba0bc40c68a8
yamaksimk <mkurakin2001@gmail.com>
2023-11-24 22:17:28 +0300

    Семинар окончен

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,1 +1,39 @@
 ﻿
+
+// Console.Write("Введите число: ");
+// int f = Convert.ToInt32(Console.ReadLine());
+// Console.Write(f + 10);
+using System.Threading.Tasks.Dataflow;
+
+// Console.Clear();
+
+// Первая задача
+
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a == b * b) 
+//     Console.WriteLine("Yes"); 
+// else
+//     Console.WriteLine("No");
+
+// Вторая задача
+
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int i = (-1) * n;
+// while (i <= n)
+// {
+//     Console.Write(i + " ");
+//     i ++;
+// }
+
+// Третья задача
+
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int n1 = n/100;
+// int n3 = n % 10;
+// Console.WriteLine(n1 + n3);
